2a47761e4fc737f07490809adda91743fb617052
Akshay Kumar <akshaykumar90@gmail.com>
2017-01-12 21:50:19 -0800

    Format code correctly

--- FontCycler.py
+++ FontCycler.py
@@ -1,11 +1,15 @@
+# -*- coding: utf-8 -*-
 from collections import namedtuple
-import sublime, sublime_plugin
 
-Field = namedtuple("Field", ["name", "default"])
+import sublime
+import sublime_plugin
+
+Field = namedtuple('Field', ['name', 'default'])
+
 
 def cycle_font(backward=False):
-    settings = sublime.load_settings("Preferences.sublime-settings")
-    settings_fonts_list = settings.get("fonts_list", [])
+    settings = sublime.load_settings('Preferences.sublime-settings')
+    settings_fonts_list = settings.get('fonts_list', [])
 
     if not settings_fonts_list:
         return
@@ -14,39 +18,45 @@ def cycle_font(backward=False):
     for item in settings_fonts_list:
         try:
             if isinstance(item, basestring):
-                item = { "font_face": item }
+                item = {
+                    'font_face': item,
+                }
         except:
             if isinstance(item, str):
-                item = { "font_face": item }
+                item = {
+                    'font_face': item,
+                }
                 
-        if "font_face" in item:
+        if 'font_face' in item:
             fonts_list.append(item)
 
     fonts_list_size = len(fonts_list)
     index_delta = -1 if backward else 1
-    current_font_face = settings.get("font_face")
+    current_font_face = settings.get('font_face')
     new_index = 0
     for index in range(fonts_list_size):
-        if fonts_list[index]["font_face"] == current_font_face:
+        if fonts_list[index]['font_face'] == current_font_face:
             new_index = (index + index_delta) % len(fonts_list)
             break
 
     font_settings = fonts_list[new_index]
     fields = [
-        Field("font_face", ""),
-        Field("font_size", settings.get("font_size")),
-        Field("line_padding_bottom", 0),
-        Field("line_padding_top", 0)
+        Field('font_face', ''),
+        Field('font_size', settings.get('font_size')),
+        Field('line_padding_bottom', 0),
+        Field('line_padding_top', 0)
     ]
     for field in fields:
         settings.set(field.name, font_settings.get(field.name, field.default))
-    sublime.save_settings("Preferences.sublime-settings")
+    sublime.save_settings('Preferences.sublime-settings')
 
-    sublime.status_message('Font Face: %s' % (font_settings["font_face"],))
+    sublime.status_message('Font Face: %s' % (font_settings['font_face'],))
+
 
 class NextFontCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         cycle_font()
+
 
 class PreviousFontCommand(sublime_plugin.TextCommand):
     def run(self, edit):
