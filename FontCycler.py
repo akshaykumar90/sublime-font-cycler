85a381d7cc9065d44271957a136408f6c9707e29
Akshay Kumar <akshaykumar90@gmail.com>
2017-02-25 22:30:58 -0800

    Fix cycle algorithm to allow the same font to appear in the font list twice at a different size

--- FontCycler.py
+++ FontCycler.py
@@ -6,6 +6,8 @@ import sublime_plugin
 
 Field = namedtuple('Field', ['name', 'default'])
 
+ANY_FONT_SIZE = -1
+
 
 def cycle_font(backward=False):
     settings = sublime.load_settings('Preferences.sublime-settings')
@@ -15,7 +17,8 @@ def cycle_font(backward=False):
         return
 
     fonts_list = []
-    for item in settings_fonts_list:
+    position_by_key = {}
+    for pos, item in enumerate(settings_fonts_list):
         try:
             if isinstance(item, basestring):
                 item = {
@@ -29,22 +32,26 @@ def cycle_font(backward=False):
                 
         if 'font_face' in item:
             fonts_list.append(item)
+            key = (item['font_face'], item.get('font_size', ANY_FONT_SIZE))
+            key_any = (item['font_face'], ANY_FONT_SIZE)
+            position_by_key[key_any] = position_by_key[key] = pos
 
-    fonts_list_size = len(fonts_list)
-    index_delta = -1 if backward else 1
+    delta = -1 if backward else 1
     current_font_face = settings.get('font_face')
-    new_index = 0
-    for index in range(fonts_list_size):
-        if fonts_list[index]['font_face'] == current_font_face:
-            new_index = (index + index_delta) % len(fonts_list)
-            break
+    current_font_size = settings.get('font_size')
+    cur_pos = position_by_key.get(
+        (current_font_face, current_font_size)
+    ) or position_by_key.get(
+        (current_font_face, ANY_FONT_SIZE)
+    )
+    new_pos = (cur_pos + delta) % len(fonts_list) if cur_pos is not None else 0
 
-    font_settings = fonts_list[new_index]
+    font_settings = fonts_list[new_pos]
     fields = [
         Field('font_face', ''),
         Field('font_size', settings.get('font_size')),
-        Field('line_padding_bottom', 0),
-        Field('line_padding_top', 0)
+        Field('line_padding_bottom', settings.get('line_padding_bottom')),
+        Field('line_padding_top', settings.get('line_padding_top'))
     ]
     for field in fields:
         settings.set(field.name, font_settings.get(field.name, field.default))
